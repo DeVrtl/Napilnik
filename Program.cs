26d93cca46c062523e7a07ddac381c5493d5c2e5
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-11-25 17:43:59 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Napilnik1
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullet;
+        private int _minBullets;
+
+        private void BulletsCheaker()
+        {
+            if (_bullet < _minBullets) 
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Fire(Player player)
+        {
+            player.TakeDamage(_damage);
+            _bullet -= 1;
+        }
+    }
+
+    class Player
+    {
+        private int _maxHealth;
+        private int _minHealth;
+
+        public int Health { get; private set; }
+
+        private void HealthCheaker()
+        {
+            if (Health > _maxHealth || Health < _minHealth)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void TakeDamage(int damge)
+        {
+            Health -= damge;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        private void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
