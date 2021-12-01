e760ed4d3c79cfd67d59cb538135aa08a827e5e2
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-12-01 20:31:36 +0500

    init

---
+++ ExampleTask2.cs
@@ -0,0 +1,14 @@
+﻿namespace Napilnik1
+{
+    class ExampleTask2
+    {
+        public static int FindCorrectArrayIndex(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
