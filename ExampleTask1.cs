4fb2ff42e332727cdf49dd7685e668fd394b5a40
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-12-01 20:44:31 +0500

    init

---
+++ ExampleTask1.cs
@@ -0,0 +1,15 @@
+﻿namespace Napilnik1
+{
+    class ExampleTask1
+    {
+        public static int ReturnValidNumber(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+}
