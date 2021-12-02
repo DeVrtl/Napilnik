e830e1fc9be1ff5d434e76a0f1794dbb11b39223
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-12-02 19:35:52 +0500

    init

---
+++ ExampleTask17.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik1
+{
+    class ExampleTask17
+    {
+        public static void GenerateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void DefineChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int PaySalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
