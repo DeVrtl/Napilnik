fed0893384c2b1e9458fe6d1dcec25a550acfc04
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-12-02 19:15:47 +0500

    init

---
+++ ExampleTask15.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Napilnik1
+{
+    class PlayerInvetory { }
+    class Gun { }
+    class TargetFollower { }
+    public class Unit{}
+    class GetUnit
+    {
+        public IReadOnlyCollection<Unit> UnitsToGet { get; private set; }
+    }
+
+}
