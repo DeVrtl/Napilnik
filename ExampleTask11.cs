f3d36cbd20ec70cf7ab12c5ef1aa75fb08acdf51
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-12-02 18:49:47 +0500

    init

---
+++ ExampleTask11.cs
@@ -0,0 +1,9 @@
+﻿namespace Napilnik1
+{
+    class ExampleTask11
+    {
+        private readonly int _iArmySize = 10;
+        private readonly int _coinsNumber = 10;
+        private readonly string _name = "Vladislav";
+    }
+}
